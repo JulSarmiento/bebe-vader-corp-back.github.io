dcbf43c6e77c98bb7a7a41e804303692675a177b
JulSarmiento <jasa1999@hotmail.com>
2020-08-26 20:05:51 -0500

    se importaron express

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const Express = require('express');
 const app = new Express();
+
+app.use(Express.json());
+app.use(Express.urlencoded());
+
+app.use(require('./src/routes'));
+
+app.listen(3000, () => {
+    console.log('El gatito nació.')
+})
---
+++ src/modules/class.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(id, pasword, userName, amount){
+        this.id = id;
+        this.pasword = pasword;
+        this.userName = userName;
+        this.amount = amount;
+    }
+
+}
